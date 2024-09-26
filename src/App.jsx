4c7682fd1f9ab5a29b3c9b6bfeca669d3daf0c93
Omer Miloh <omerrob11@gmail.com>
2024-09-26 12:18:09 +0300

    feat: passing title prop to cvShow

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import ShowCv from "./components/CvShow";
 import PersonalInfo from "./components/Personal-info";
 
 function App() {
-  const [personalData, setPersonalData] = useState({
-    name: "",
-    email: "",
-    tel: "",
-  });
+  // it's an array at the end - so, needs to changes it to array.
+  // empty array could do it.
+  // this is empty array for the PesronalData on save
+  // in the pesronal data, the state is for the inputs
+  const [personalData, setPersonalData] = useState([]);
 
   function handlePersonalInfoChanges(data) {
-    // data should be a new object, so we are not mutating state
     console.log(data);
+    // data should be a new object, so we are not mutating state
     setPersonalData(data);
   }
 
   return (
     <>
+      <ShowCv personalData={personalData} title="Personal Information" />
       <PersonalInfo handlePersonalInfoChanges={handlePersonalInfoChanges} />
     </>
   );
