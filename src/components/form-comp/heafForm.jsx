13b4becb5d5a472a3f8b61df1ec6ef6f7cddda48
Omer Miloh <omerrob11@gmail.com>
2024-09-26 09:37:51 +0300

    feat:add toggle for input fileds component based on arrow state

--- src/components/form-comp/heafForm.jsx
+++ src/components/form-comp/heafForm.jsx
@@ -1,9 +1,20 @@
-function FormHeader({ icon, title }) {
+import { useState } from "react";
+
+function FormHeader({ icon, title, setIsArrowActive, isArrowActive }) {
+  function handleArrowToggle(event) {
+    console.log(event);
+    !isArrowActive ? setIsArrowActive(true) : setIsArrowActive(false);
+  }
+
   return (
     <div className="form_head">
       <div className="heading_icon">{icon}</div>
       <h2 className="form_title">{title}</h2>
-      <i className="fas fa-chevron-down img arrow"></i>;
+      <i
+        className="fas fa-chevron-down img arrow"
+        onClick={handleArrowToggle}
+      ></i>
+      ;
     </div>
   );
 }
