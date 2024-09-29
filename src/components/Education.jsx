f1668d6d3bad25d518c2bf664bde74aa6b26555d
Omer Miloh <omerrob11@gmail.com>
2024-09-29 08:30:07 +0300

    feat: add inputs state to Education comp

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,4 +1,12 @@
 export default function Education() {
+  const [inputValues, setInputValues] = useState([
+    { degree: "", id: crypto.randomUUID() },
+    { school: "", id: crypto.randomUUID() },
+    { city: "", id: crypto.randomUUID() },
+    { country: "", id: crypto.randomUUID() },
+    { startDate: "", id: crypto.randomUUID() },
+    { endDate: "", id: crypto.randomUUID() },
+  ]);
   return (
     <div className="education_container">
       <h1>This is all about eudcation</h1>
