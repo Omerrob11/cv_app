a5300a4357ec6949f7e9ee0a5bded06ac4b5185c
Omer Miloh <omerrob11@gmail.com>
2024-09-30 10:25:15 +0300

    feat: add intial inputs for expirence comp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,3 +4,13 @@ import FormInputs from "./form-comp/formInputs";
 import { SaveButton } from "./form-comp/buttons";
 import Footer from "./form-comp/footerForm";
 import "../styles/form-style.css";
+
+function Experience() {
+  const intialInputs = [
+    { jobTitle: "", id: crypto.randomUUID() },
+    { company: "", id: crypto.randomUUID() },
+    { startDate: "", id: crypto.randomUUID() },
+    { endDate: "", id: crypto.randomUUID() },
+    { description: "", id: crypto.randomUUID() },
+  ];
+}
