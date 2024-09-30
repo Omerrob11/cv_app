11541f67bc2714c18f1cc788ee5ef24f3d77dd71
Omer Miloh <omerrob11@gmail.com>
2024-09-30 10:18:26 +0300

    refactor: change app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 import ShowCv from "./components/CvShow";
-import PersonalInfo from "./components/Personal-info";
 import Education from "./components/Education";
+import PersonalInfo from "./components/Personal_Info";
 
 function App() {
   // it's an array at the end - so, needs to changes it to array.
   // empty array could do it.
   // this is empty array for the PesronalData on save
   // in the pesronal data, the state is for the inputs
-  const [personalData, setPersonalData] = useState([]);
   const [educationData, setEducationData] = useState([]);
+  const [personalData2, setPersonalData2] = useState([]);
 
   function handleEducationInfoChanges(data) {
     // assumming we get a new reference, not the actual state
     setEducationData(data);
   }
 
-  function handlePersonalInfoChanges(data) {
+  function handlePersonalInfoChanges2(data) {
     // data should be a new object, so we are not mutating state
-    setPersonalData(data);
+    setPersonalData2(data);
   }
 
   return (
     <>
-      {personalData.length > 0 && (
-        <ShowCv compData={personalData} title="Personal Information" />
-      )}
       {educationData.length > 0 && (
         <ShowCv compData={educationData} title="Education Information" />
       )}
 
-      <PersonalInfo handlePersonalInfoChanges={handlePersonalInfoChanges} />
       <Education handleEducationInfoChanges={handleEducationInfoChanges} />
+      <PersonalInfo handlePersonalInfoChanges={handlePersonalInfoChanges2} />
+      {personalData2.length > 0 && (
+        <ShowCv compData={personalData2} title="Personal Information 2" />
+      )}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from "react-dom/client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./App";
-import PersonalInfo from "./components/Personal-info";
 import "./index.css";
-function RenderContent(props) {
-  console.log(props);
-  return <div></div>;
-}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
