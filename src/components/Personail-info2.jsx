1218c5181fa5d3b0f1a9791ccdd23f90cc9c7579
Omer Miloh <omerrob11@gmail.com>
2024-09-30 10:15:30 +0300

    style: refactor app style

--- src/components/Personail-info2.jsx
+++ src/components/Personail-info2.jsx
@@ -3,6 +3,7 @@ import FormHeader from "./form-comp/heafForm";
 import FormInputs from "./form-comp/formInputs";
 import { SaveButton } from "./form-comp/buttons";
 import Footer from "./form-comp/footerForm";
+import "../styles/form-style.css";
 
 function PersonalInfo2({ handlePersonalInfoChanges }) {
   const initialInputs = [
